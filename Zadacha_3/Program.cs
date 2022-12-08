f655b052bd860982260c1121da19fdb1e0428455
OLGA DRACH <dolchik88@mail.ru>
2022-12-08 18:17:11 +0300

    if с двумя условиями

---
+++ Zadacha_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход число 
+// и проверяет, кратно ли оно одновременно 7 и 23.
+
+// 14 -> нет 
+// 46 -> нет 
+// 161 -> да
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine());
+
+if ((number % 7 ==0) && (number % 23 ==0))
+    Console.WriteLine($"{number} -> да");
+else
+    Console.WriteLine($"{number} -> нет");
+
