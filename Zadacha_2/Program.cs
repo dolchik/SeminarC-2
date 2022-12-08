cf5c6d482ea3cd782cb3c7ed35e09b1749b5b110
OLGA DRACH <dolchik88@mail.ru>
2022-12-08 18:04:52 +0300

    Сравнение двух чисел на кратность

---
+++ Zadacha_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая будет принимать на вход два числа 
+// и выводить, является ли второе число кратным первому. 
+// Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+// 34, 5 -> не кратно, остаток 4 
+// 16, 4 -> кратно
+
+Console.Write("Введите первое число: ");
+int first = int.Parse(Console.ReadLine()); // парсим ответ
+Console.Write("Введите второе число: ");
+int second = int.Parse(Console.ReadLine());
+int ostatok = first % second;               // переменная с остатком от деления
+
+if (first % second ==0)
+    Console.WriteLine($"{first}, {second} -> кратно");
+else 
+    Console.WriteLine($"не кратно, остаток {ostatok}");
