d7e84fd04e0ac3751b9a878a961e6e64abf97ccd
OLGA DRACH <dolchik88@mail.ru>
2022-12-08 17:28:54 +0300

    Поиск крайних чисел трехзначного числа

---
+++ Zadacha_1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Напишите программу, которая выводит случайное трехзначное число 
+//и удаляет вторую цифру этого числа.
+
+Console.Clear();
+
+int randomN = new Random().Next(100, 1000); //переменная со случайным числом от 100 до 999
+
+// Длинное решение!
+// Console.WriteLine($"Случайное число: {randomN}");
+
+// int ostatok = randomN % 10;
+// Console.WriteLine($"Последняя цифра: + {ostatok}");
+
+// int first = randomN / 100;
+// Console.WriteLine($"Первая цифра: + {first}");
+
+// Console.WriteLine($"Первая и последня цифры: {first}{ostatok}");
+
+// Короткое решение
+
+int result = (randomN / 100) * 10 + randomN % 10;
+Console.WriteLine(randomN + " -> " + result);
