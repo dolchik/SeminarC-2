6a26889f0d33cdfd2550793d83ae3d03638135e6
OLGA DRACH <dolchik88@mail.ru>
2022-12-08 16:22:00 +0300

    Поиск наибольшего из сотсавляющих одного числа

---
+++ Zadacha_0/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+int randomN = new Random().Next(10, 100); // случайное число от 10 до 99
+Console.WriteLine("Случайное число: " + randomN);
+
+int ostatok = randomN%10;
+Console.WriteLine("Последняя цифра: " + ostatok);
+
+int first = randomN / 10;
+Console.WriteLine("Первая цифра: " + first);
+
+int max = first;
+
+if (ostatok > first) max = ostatok;
+Console.WriteLine("Максимум: " + max);
